2dd64fd87e82f9464ec22f3a8f23ea26f849513e
Mavaddat <amir.smir.1389121@gmail.com>
2026-09-17 13:53:29 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = "modat-accounting-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", function(event) {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME).then(function(cache) {
+
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", function(event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function(cacheNames) {
+
+            return Promise.all(
+
+                cacheNames.map(function(cacheName) {
+
+                    if (cacheName !== CACHE_NAME) {
+
+                        return caches.delete(cacheName);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", function(event) {
+
+    event.respondWith(
+
+        fetch(event.request)
+            .then(function(response) {
+
+                return response;
+
+            })
+            .catch(function() {
+
+                return caches.match(event.request);
+
+            })
+
+    );
+
+});
